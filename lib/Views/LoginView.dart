0eb4e5e8e63971e022f3c1a6f9242e141a9bff91
dalesius <dali.molochnik@gmail.com>
2021-03-16 15:30:27 +0100

    feat: add login view

---
+++ lib/Views/LoginView.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:yeet/yeet.dart';
+
+class LoginView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              context.yeet();
+            },
+          ),
+          title: Text('Login')),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 450, minWidth: 250),
+          child: LoginWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginWidget extends StatelessWidget {
+  const LoginWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Please enter your email',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Please enter your password',
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
