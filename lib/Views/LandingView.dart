0eea417fe3bdeb7f94ec75950f39d1c7f3d487ae
dalesius <dali.molochnik@gmail.com>
2021-03-16 15:31:51 +0100

    fix: modify landing view's name and structure

--- lib/Views/LandingView.dart
+++ lib/Views/LandingView.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yeet/yeet.dart';
 
-class Home extends StatelessWidget {
-  Home({
-    required this.title,
-  });
-
-  final String title;
-
+class LandingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey.shade600,
       body: Center(
         child: Container(
           width: 400,
@@ -20,13 +14,15 @@ class Home extends StatelessWidget {
               FlutterLogo(size: 200.0),
               Text(
                 'TourMe!',
-                style: TextStyle(fontSize: 30, color: Colors.white),
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
               SizedBox(height: 30.0),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () => context.yeet('/login'),
                   child: Text('Login'),
                 ),
               ),
@@ -34,7 +30,7 @@ class Home extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () => context.yeet('/register'),
                   child: Text('Register'),
                 ),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Screens/Home.dart';
+import 'package:yeet/yeet.dart';
+import 'config/routes.dart';
 
 void main() {
   runApp(App());
@@ -8,12 +9,14 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationParser: YeetInformationParser(),
+      routerDelegate: YeeterDelegate(yeet: yeet),
       title: 'TourMe!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        backgroundColor: Colors.blueGrey.shade600,
       ),
-      home: Home(title: 'TourMe! Home Page'),
     );
   }
 }
