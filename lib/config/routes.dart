e83a98d8f9ca6b51e384e47d4a88aa6693505480
dalesius <dali.molochnik@gmail.com>
2021-03-16 15:32:26 +0100

    feat: add routes manager and yeet dependency

---
+++ lib/config/routes.dart
@@ -0,0 +1,23 @@
+import 'package:tour_me/Views/LandingView.dart';
+import 'package:tour_me/Views/LoginView.dart';
+import 'package:tour_me/Views/RegisterView.dart';
+import 'package:yeet/yeet.dart';
+
+final yeet = Yeet(
+  children: [
+    Yeet(
+      path: '/',
+      builder: (_, __) => LandingView(),
+      children: [
+        Yeet(
+          path: '/login',
+          builder: (params, _) => LoginView(),
+        ),
+        Yeet(
+          path: '/register',
+          builder: (_, __) => RegisterView(),
+        ),
+      ],
+    ),
+  ],
+);
